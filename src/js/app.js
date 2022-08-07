78e79a689d7a1320da42e786d57e716fde982814
theenutt <you@example.com5107624@gmail.com>
2022-08-08 01:21:54 +0300

    add faq section with js

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,16 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
+
+
+
+$(document).ready(function() {
+    $('.faq__question').click(function(){
+        if($('.faq__spoilers').hasClass('one')){
+            $('.faq__question').not($(this)).removeClass('active');
+            $('.faq__answer').not($(this).next()).slideUp(300);
+        }
+        $(this).toggleClass('active').next().slideToggle(300);
+
+    });
+});
